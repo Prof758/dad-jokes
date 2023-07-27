997ab743585cbbd47a372c756349c8fe8cb35c5d
Simon Prophet <simon.s.prophet@outlook.com>
2023-07-27 19:32:06 +0100

    add console.log(jsonData)

--- script.js
+++ script.js
@@ -7,11 +7,6 @@ generateJoke();
 
 async function generateJoke() {
   const config = {
-    // method: 'GET',
-    // headers: {
-    //   'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
-    //   'X-RapidAPI-Host': 'healthruwords.p.rapidapi.com',
-    // },
     headers: {
       Accept: 'application/json',
     },
@@ -19,14 +14,7 @@ async function generateJoke() {
   const response = await fetch('http://icanhazdadjoke.com', config);
   const jsonData = await response.json();
   jokeEl.innerHTML = jsonData.joke;
-
-  // const response = await fetch(
-  //   'https://healthruwords.p.rapidapi.com/v1/quotes/?t=Wisdom&maxR=1&size=medium&id=731',
-  //   config
-  // );
-  // const jsonData = await response.json();
-  // console.log(jsonData);
-  // jokeEl.innerHTML = jsonData.text;
+  console.log(jsonData);
 }
 
 btn.addEventListener('click', generateJoke);
